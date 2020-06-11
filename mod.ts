7142baee50d977e8ea5174e4c092793db50c0223
petesamper <petesamper@gmail.com>
2020-06-11 14:04:06 -0400

    Finds earth-like planets and relevant data.

--- mod.ts
+++ mod.ts
@@ -3,6 +3,9 @@ import { BufReader } from "https://deno.land/std/io/bufio.ts";
 // ^^^ before sending data back byte-by-byte, buffers(waits), sends back (ex.): full line of data.
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
+// Lodash
+import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+
 interface Planet {
   [key: string]: string;
 }
@@ -20,7 +23,7 @@ async function loadPlanetsData() {
   Deno.close(file.rid);
 
   // confirmed exoplanets:
-  const confirmed = (allExoplanets as Array<Planet>).filter((planet) => {
+  const confirmedPlanets = (allExoplanets as Array<Planet>).filter((planet) => {
     const planetaryRadius = Number(planet["koi_prad"]);
     const stellarMass = Number(planet["koi_smass"]);
     const stellarRadius = Number(planet["koi_srad"]);
@@ -31,9 +34,20 @@ async function loadPlanetsData() {
       stellarRadius > 0.99 && stellarRadius < 1.01;
   });
 
-  return confirmed;
+  return confirmedPlanets.map((planet) => {
+    return _.pick(planet, [
+      "koi_prad",
+      "koi_smass",
+      "koi_srad",
+      "kepler_name",
+      "koi_count",
+      "koi_steff",
+    ]);
+  });
 }
 
 const earthLikePlanets = await loadPlanetsData();
-console.log(earthLikePlanets);
+for (const planet of earthLikePlanets) {
+  console.log(planet);
+}
 console.log(`${earthLikePlanets.length} habitable planets found.`);
